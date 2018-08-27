670f5e667f315a9bfb2eb9d415186cf6cd80291e
Kalarrs Topham <k-dawg-klark@hotmail.com>
2018-08-27 07:32:13 -0700

    Support 0, 1, or 2 arguments in HTTP method. Match AWS functionality. (#1)
    * KAL-19 Updated local to support 0-2 arg signatures
    
    * KAL-19 Updated message, ran tests on AWS.
    
    * bump

--- Kalarrs.Serverless.NetCore.Util/RequestDelegates/ApiGateway.cs
+++ Kalarrs.Serverless.NetCore.Util/RequestDelegates/ApiGateway.cs
@@ -17,12 +17,20 @@ namespace Kalarrs.Serverless.NetCore.Util.RequestDelegates
         {
             return async (context) =>
             {
+                if (parameterInfos.Count > 2) throw new Exception("Unrecongnized paramter count for HTTP method. Expected 2 paramters. (object event, ILambdaContext context)");
+                // NOTE: KAL-19 unable to check paramter 2 to see if it ILambdaContext. Will allow (object e, object context) which won't work on AWS but will work here.
+                // tried https://stackoverflow.com/questions/4963160/how-to-determine-if-a-type-implements-an-interface-with-c-sharp-reflection
+                
                 APIGatewayProxyResponse response;
                 var apiGatewayProxyRequest = await context.ToApiGatewayProxyRequest(httpConfig.Path).ConfigureAwait(false);
 
                 EnvironmentVariable.PrepareEnvironmentVariables(defatultEnvironmentVariables, serverlessEnvironmentVariables,httpConfig.Environment);
-                var handlerResponse = handlerMethod.Invoke(handler, new object[] {apiGatewayProxyRequest, new TestLambdaContext()});
 
+                var args = new List<object> { };
+                if (parameterInfos.Count > 0) args.Add(apiGatewayProxyRequest);
+                if (parameterInfos.Count > 1) args.Add(new TestLambdaContext());
+                
+                var handlerResponse = handlerMethod.Invoke(handler, parameterInfos.Count == 0 ? null : args.ToArray());
                 if (handlerResponse is Task<APIGatewayProxyResponse> task) response = await task.ConfigureAwait(false);
                 else if (handlerResponse is APIGatewayProxyResponse proxyResponse) response = proxyResponse;
                 else throw new Exception("The Method did not return an APIGatewayProxyResponse.");
